1f3fbe3a51c9204bd10bab9a67d95766c05620b9
Rafael de Oliveira <oliver.rafa201@gmail.com>
2024-01-12 01:44:14 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Wep_app_tripleten
 Project Dashboard Tripleten
+O aplicativo web desenvolvido neste projeto tem como objetivo facilitar a análise e visualização de dados relacionados a anúncios de vendas de carros.
--- app.py
+++ app.py
@@ -2,30 +2,33 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('../vehicles.csv')  # lendo os dados
-hist_button = st.button('Criar histograma')  # criar um botão
+# Lendo os dados
+car_data = pd.read_csv('../vehicles.csv')
 
-if hist_button:  # se o botão for clicado
-    # escrever uma mensagem
+# Botão para criar histograma
+hist_button = st.button('Criar histograma')
+
+if hist_button:
+    # Mensagem
     st.write(
         'Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
 
-    # criar um histograma
+    # Criar histograma
     fig = px.histogram(car_data, x="odometer")
 
-    # exibir um gráfico Plotly interativo
+    # Exibir gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
 
-disp_button = st.button('cria gráfico de dispersão')  # cria um botão
+# Botão para criar gráfico de dispersão
+disp_button = st.button('Criar gráfico de dispersão')
 
-if disp_button:  # se o botão for clicado
-    # escrever uma mensagem
+if disp_button:
+    # Mensagem
     st.write(
         'Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
 
-    # criando gráfico de dispersão
-    # criar um gráfico de dispersão
+    # Criar gráfico de dispersão
     fig = px.scatter(car_data, x="odometer", y="price")
 
-    # exibir um gráfico interativo
+    # Exibir gráfico interativo
     st.plotly_chart(fig, use_container_width=True)
